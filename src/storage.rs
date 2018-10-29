05238484b925e24d4e6d04836368ad53f0618c5d
EricGz <zgz_14@126.com>
2018-10-29 09:12:25 +0800

    modified storage and msg to deal with receiving message

--- src/storage.rs
+++ src/storage.rs
@@ -1,122 +1,148 @@
-//This module is responsible for static file processing.
-//
-//
-//Declares rules for modifying local data and provides data interfaces.
-//
-//
-//mainly relating to self discovery function and cluster maintenance.
 #[macro_use]
 extern crate serde_derive;
-
 extern crate toml;
-extern crate serde;
-extern crate serde_json;
+extern crate chrono;
 
-use std::path::Path;
+use chrono::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize)]
+#[derive(Serialize)]
+#[derive(Debug)]
 pub struct Node {
     id: String,
     ip: String,
+    hostname: String,
     timestamp: String,
 }
-impl Node {
-    fn new(_id: &str, _ip: &str, _timestamp: &str) -> Node {
-        Node {
-            id: _id.to_string(),
-            ip: _ip.to_string(),
-            timestamp: _timestamp.to_string(),
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+#[derive(Serialize)]
+pub struct NodeConfig
+{
+    changetime: String,
+    node: Vec<Node>
+}
+impl Node{
+    fn new(id: String, ip: String, hostname: String, timestamp: String) -> Node {
+        Node {id,ip,hostname,timestamp,}
+    }
+    fn default() -> Node{
+        Node{
+            id: String::from("default id"),
+            ip: String::from("default ip"),
+            hostname: String::from("default hostname"),
+            timestamp: String::from("default timestamp"),
         }
     }
 }
 
-
 // this function get a filename, return a vector of Nodes from the file.
-pub fn get_list() -> Vec<Node> {
+pub fn get_list() -> (String , Vec<Node>) {
 
-    /*open the file; exception handling*/
-    let filename = "data.toml";
-    let pathname = "../data/".to_owned()+filename;
-    let path = Path::new(&pathname);
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(e) => panic!("couldn't open {}, exception: {}", path.display(), e)
-    };
+    let file_path = "data/AliveList.toml";
+    let mut file = File::open(file_path).expect("file not opened");
 
-    let mut str_val = String::new();
-    match file.read_to_string(&mut str_val) {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't read {}, exception: {}", path.display(), e)
-    };
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
 
-    let nodes: Vec<Node> = serde_json::from_str(&str_val).unwrap();
-    
-    nodes
+    let config: NodeConfig = toml::from_str(&contents).unwrap();
+    /*println!("{}", config.changetime);
+    let nodelist : Vec<Node> = config.node;
+    for item in nodelist {
+         println!("{:?}", item);
+    };*/
+    (config.changetime , config.node)
+
 }
 
 
 // this function get a vector of Nodes and a filename, write this vector into the file.
-pub fn write_list(v: Vec<Node>) {
+pub fn write_list(nodelist:Vec<Node>) {
 
-    /*open the file; exception handling*/
-    let filename = "data.toml";
-    let pathname = "../data/".to_owned()+filename;
-    let path = Path::new(&pathname);
-    let path = Path::new(path);
-    let mut file = match File::create(path) {
-        Ok(file) => file,
-        Err(e) => panic!("couldn't open {}, exception: {}", path.display(), e)
+    let file_path = "data/AliveList.toml";
+    let mut file = File::create(file_path).expect("file not created");
+
+    let changetime = Local::now();
+    let schangetime :String = changetime.timestamp_millis().to_string();
+    let config = NodeConfig{
+        changetime: schangetime,
+        node: nodelist,
     };
+    let toml = toml::to_string(&config).unwrap();
+    file.write(toml.as_bytes()).expect("file not write");
 
-    /*serialize and write to file*/
-    let mut serialized = serde_json::to_string(&v).unwrap();
-    match file.write_all(serialized.as_bytes()) {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't write {}, exception: {}", path.display(), e)
-    }
+    //
+    println!("Send List_msg to tell others the change.");
 }
 
+// this function get a Node and a filename, change the Node.
+pub fn alive_deal(alive_node: Node) {
 
-// this function get a Node and a filename, change the Node's timestamp in the file into "***".
-pub fn dead_deal(v: Node) {
-    /*open the file; exception handling*/
-    let filename = "data.toml";
-    let pathname = "../data/".to_owned()+filename;
-    let path = Path::new(&pathname);
-    let path = Path::new(path);
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(e) => panic!("couldn't open {}, exception: {}", path.display(), e)
-    };
-    let mut str_val = String::new();
-    match file.read_to_string(&mut str_val) {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't read {}, exception: {}", path.display(), e)
-    };
+    let file_path = "data/AliveList.toml";
+    let mut file = File::open(file_path).expect("file not opened");
 
-    /* find the dead node via id and change its timestamp into '***' */
-    let mut nodes: Vec<Node> = serde_json::from_str(&str_val).unwrap();
-    let mut new_nodes: Vec<Node> = Vec::new();
-    for mut node in nodes {
-        if node.id == v.id {
-            let mut new_node: Node = Node::new(&node.id.clone(), &node.ip.clone(), &"***".to_string());
-            new_nodes.push(new_node);
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let config: NodeConfig = toml::from_str(&contents).unwrap();
+
+    /* find the alive node via id and change its Node */
+    let nodelist: Vec<Node> = config.node;
+    let mut newlist: Vec<Node> = Vec::new();
+    for mut item in nodelist {
+        if item.id == alive_node.id {
+            let new_node = Node{
+                id : alive_node.id.clone(),
+                ip : alive_node.ip.clone(),
+                hostname : alive_node.hostname.clone(),
+                timestamp : alive_node.timestamp.clone(),
+            };
+            newlist.push(new_node);
         }
         else {
-            new_nodes.push(node);
+            newlist.push(item);
         }
     }
 
     /* write the new Vec<Node> into file */
-    file = match File::create(path) {
-            Ok(file) => file,
-            Err(e) => panic!("couldn't open {}, exception: {}", path.display(), e)
-        };
-    let mut serialized = serde_json::to_string(&new_nodes).unwrap();
-    match file.write_all(serialized.as_bytes()) {
-        Ok(s) => s,
-        Err(e) => panic!("couldn't write {}, exception: {}", path.display(), e)
+    write_list(newlist);
+}
+
+// this function get a Node and a filename, change the Node's timestamp in the file into "***".
+pub fn dead_deal(dead_node: Node) {
+
+    let file_path = "data/AliveList.toml";
+    let mut file = File::open(file_path).expect("file not opened");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let config: NodeConfig = toml::from_str(&contents).unwrap();
+
+    /* find the dead node via id and change its timestamp into '***' */
+    let nodelist: Vec<Node> = config.node;
+    let mut newlist: Vec<Node> = Vec::new();
+    for mut item in nodelist {
+        if item.id == dead_node.id {
+            let new_node = Node{
+                id : dead_node.id.clone(),
+                ip : dead_node.ip.clone(),
+                hostname : dead_node.hostname.clone(),
+                timestamp : String::from("***"),
+            };
+            newlist.push(new_node);
+        }
+        else {
+            newlist.push(item);
+        }
     }
+
+    /* write the new Vec<Node> into file */
+    write_list(newlist);
 }
